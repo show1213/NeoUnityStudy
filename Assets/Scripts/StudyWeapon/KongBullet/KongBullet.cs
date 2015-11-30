f8c8b46b0c9f32788a1e9690f17873212c7099ae
show1213 <show_1213@naver.com>
2015-12-01 02:28:22 +0900

    ddd
    dd

---
+++ Assets/Scripts/StudyWeapon/KongBullet/KongBullet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class KongBullet : MonoBehaviour
+{
+    private Vector3 vDir;
+    private DateTime createTime;
+    private float fSpeed = 2f;
+    private int nDestroyTime = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        createTime = DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Init(Vector3 _vPos, Vector3 _vDir)
+    {
+        transform.localPosition = _vPos;
+        vDir = _vDir;
+    }
+
+    void Move()
+    {
+        transform.Translate(vDir * fSpeed * Time.deltaTime);
+
+        TimeSpan ts = DateTime.Now - createTime;
+        if (ts.Seconds >= nDestroyTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
