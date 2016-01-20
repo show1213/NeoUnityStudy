6a24ccd47384c4dfae20b5abc469b0f73d5e2102
nolleh <nolleh7707@gmail.com>
2016-01-20 22:10:52 +0900

    Server manager

---
+++ Assets/Scripts/Network/ServerManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerManager : MonoBehaviour {
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
+
+	public Transform PlayerPrefab;
+	// Use this for initialization
+	void Start () {
+		StartServer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 8837, !Network.HavePublicAddress());
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initilaized");
+		Network.Instantiate(PlayerPrefab, new Vector3(0,0,0), transform.rotation, 0);
+	}
+
+	void OnConnectedToServer()
+	{
+		Debug.Log("connected");
+	}
+
+	//void On
+
+	void OnPlayerConnected(NetworkPlayer player)
+	{
+		Debug.Log("player :" + player.ipAddress + ", player id:" + player);
+	}
+
+	void OnPlayerDisconnected(NetworkPlayer player)
+	{
+	 	Debug.Log("Clean up after player :" + player.ipAddress + ", player id: " + player);
+        Network.RemoveRPCs(player);
+	 	Network.DestroyPlayerObjects(player);
+	}
+}
